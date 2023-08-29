005cc33f890e69bbe39922aeacb65868648c44ca
Lukas Eichel <lukas.eichel@ovgu.de>
2023-08-29 13:43:11 +0200

    finished substitution back/ and forward

--- src/main/scala/solver/Substitution.scala
+++ src/main/scala/solver/Substitution.scala
@@ -4,7 +4,7 @@ import scala.reflect.ClassTag
 
 class Substitution {
   
-    def backward_sub(A: Matrix[Double], b: Array[Double]) : Array[Double] = {
+    def forward_sub(A: Matrix[Double], b: Array[Double]) : Array[Double] = {
         val (n, m): (Int, Int) = A.getSize()
         if (n != m) throw new IllegalArgumentException("matrix must be square")
 
@@ -13,6 +13,23 @@ class Substitution {
         for (k <- 0 until n) {
             var sum: Double = 0.0
             for (i <- 0 until k - 1) {
+                sum = sum + (x(i) * A.at(k, i)) 
+            }
+            val x_k: Double = (b(k) - sum) / A.at(k, k)  
+            x(k) = x_k
+        }
+        return x;
+    }
+
+    def backward_sub(A: Matrix[Double], b: Array[Double]) : Array[Double] = {
+        val (n, m): (Int, Int) = A.getSize()
+        if (n != m) throw new IllegalArgumentException("matrix must be square")
+
+        var x: Array[Double] = new Array[Double](n)
+
+        for (k <- (n - 1 to 0 by -1)) {
+            var sum: Double = 0.0
+            for (i <- (n - 1 to k by -1)) {
                 sum = sum + (x(i) * A.at(k, i)) 
             }
             val x_k: Double = (b(k) - sum) / A.at(k, k)  
--- src/test/scala/solver/TestSubstitution.scala
+++ src/test/scala/solver/TestSubstitution.scala
@@ -2,7 +2,7 @@ package solver
 import core.Matrix;
 
 class TestSubstitution extends munit.FunSuite {
-    test("backwards substitution") {
+    test("backward substitution") {
         val matrix: Matrix[Double] = Matrix[Double](2, 2)
         matrix.init(1.0 :: 2.0 :: 3.0 :: 5.0 :: Nil)
         val b: Array[Double] = Array(1, 3)
@@ -11,5 +11,33 @@ class TestSubstitution extends munit.FunSuite {
         assertEquals(x(0), 1.0)
         assertEquals(x(1), 0.0)
     }
-  
+
+    test("backward substitution II") {
+        val matrix: Matrix[Double] = Matrix[Double](3, 3)
+        matrix.init(2.0 :: 2.0 :: 3.0 :: 1.0 :: 2.0 :: 2.0 :: 1.0 :: 1.0 :: 4.0 :: Nil)
+        val b: Array[Double] = Array(75, 50, 50)
+        val res = Elimination().gaussian_elimination(matrix, b, false)
+        val x: Array[Double] = Substitution().backward_sub(res(0), res(1))
+        assertEquals(x.toList, Array[Double](20,10,5).toList)
+    }
+
+    test("forward substitution") {
+        val matrix: Matrix[Double] = Matrix[Double](2, 2)
+        matrix.init(1.0 :: 0.0 :: 2.0 :: 3.0 :: Nil)
+        val b: Array[Double] = Array(4, 8)
+        val x: Array[Double] = Substitution().forward_sub(matrix, b)
+        assertEquals(x(0), 4.0)
+        assertEquals(x(1), 0.0)
+    }  
 }
+
+/*
+
+1 0  4
+2 3  8
+
+2*4 + 3x = 8
+0
+
+
+*/
